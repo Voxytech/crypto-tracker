0bc5fbd0601a41aa38cc7a12224cd2c4719912cc
Doritanh <charrier.antho99@gmail.com>
2021-05-02 00:21:38 +0200

    Init project

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      hash: true,
+      base: "",
+      template: "static/index.html",
+      filename: "index.html",
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+};
